af89f399915d89e65462cb2fe2a9a710726e43e7
Kate Clark <cla16022@byui.edu>
2021-02-01 22:56:23 -0700

    Added edit_book

--- cloud_database.py
+++ cloud_database.py
@@ -5,10 +5,6 @@ import os
 
 def initialize_firestore():
 
-    # Setup Google Cloud Key
-    # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "cosmere-database-firebase-adminsdk-ekqyh-164672a1d3.json"
-
-    # Use the application default credentials
     cred = credentials.Certificate(r"C:\Users\Kate\Documents\Folders\CSE 310\cloud_database\cosmere-database-firebase-adminsdk-ekqyh-164672a1d3.json")
     firebase_admin.initialize_app(cred)
 
@@ -33,18 +29,48 @@ def display_books(db):
         books = result.to_dict()
         print("{:<25}  {:<20}  {:<25}  {:<20}  {:<20}".format(books["title"], books["author"], books["series_name"], books["num_in_series"], books["checked_out"]))
 
+def edit_book(db, title):
+    component = None
+    print()
+    print(f'Which part of "{title}" would you like to edit?')
+    print("1) Title")
+    print("2) Author")
+    print("3) Series name")
+    print("4) Number in series")
+    print("5) Checked out")
+    print("6) Quit")
+    component = input("> ")
+    print()
+    
+    while component != "6":
+        if component == "1":
+            title = input("What is the new title? ")
+        elif component == "2":
+            author = input("What is the new first and last names of the author? ")
+        elif component == "3":
+            series_name = input("What is the new series name? ")
+        elif component == "4":
+            num_in_series = input("What is the new number in the book series (1, 2, 3...)? ")
+        elif component == "5":
+            checked_out = input("Is the book checked out (y/n)? ")
+
+    # books[f"{component}"] = new_entry
+    # db.collection("books").document(title)...
+
+
 def main():
     db = initialize_firestore()
     choice = None
     while choice != "5":
         print()
         print("1) Add Book to Library")
-        print("2) Edit Book in Library")
-        print("3) Remove Book from Library")
+        print("2) Remove Book from Library")
+        print("3) Edit Book in Library")
         print("4) Display Books in Library")
         print("5) Quit")
         choice = input("> ")
         print()
+        
         if choice == "1":
             title = input("What is the book title? ")
             author = input("What is the first and last name of the author? ")
@@ -53,10 +79,14 @@ def main():
             checked_out = input("Is the book checked out (y/n)? ")
             set_book(db, title, author, series_name, num_in_series, checked_out)
             print(f'\n"{title}" has been added to library.')
-        elif choice == "3":
+        elif choice == "2":
             title = input("What is the title of the book you wish to remove? ")
             remove_book(db, title)
             print(f'\n"{title}" has been removed from library.')
+        elif choice == "3":
+            title = input("What is the title of the book entry you wish to edit? ")
+            edit_book(db, title)
+            print(f'\n"{title}" has been successfully edited.')
         elif choice == "4":
             display_books(db)
         else:
