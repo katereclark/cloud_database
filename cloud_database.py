103161b78fb282dec039e02c567a8f96d0a8ad20
Kate Clark <cla16022@byui.edu>
2021-02-05 18:28:34 -0700

    Added user functions

--- cloud_database.py
+++ cloud_database.py
@@ -8,13 +8,6 @@ def initialize_firestore():
     cred = credentials.Certificate(r"C:\Users\Kate\Documents\Folders\CSE 310\cloud_database\cosmere-database-firebase-adminsdk-ekqyh-164672a1d3.json")
     firebase_admin.initialize_app(cred)
 
-#     service cloud.firestore {  match /databases/{database}/documents {    match /books/{book} {
-#       allow read: if <condition>;
-#       allow write: if <condition>;
-#     }
-#   }
-# }
-
     db = firestore.client()
     return db
 
@@ -88,18 +81,68 @@ def display_books_by_series(db, series_name):
         books = result.to_dict()
         print("{:<25}  {:<20}  {:<25}  {:<20}  {:<20}".format(books["title"], books["author"], books["series_name"], books["num_in_series"], books["checked_out"]))
 
-def main():
-    db = initialize_firestore()
+# Adds a user to the users collection.
+def set_user(db, username, librarian, patron):
+    user = {"librarian" : librarian, "patron" : patron}
+    db.collection("users").document(username).set(user)
 
+# Deletes a user from the users collection.
+def remove_user(db, username):
+    db.collection("users").document(username).delete()
+
+# Edites a user field for the specified username.
+def edit_user(db, username):
+    property = None
+    print()
+    print(f"Which part of the user's information would you like to edit?")
+    print("1) Username")
+    print("2) Librarian access")
+    print("3) Patron access")
+    print("4) Quit")
+    property = input("> ")
+    print()
+    
+    entry_ref = db.collection("users").document(username)
+
+    while property != "4":
+        if property == "1":
+            print("Please delete the user and then create them again under the desired username.")
+            break
+        elif property == "2":
+            librarian = input("Will this user have libarian access (yes or no)? ")
+            if librarian == "no":
+                librarian = False
+            else:
+                librarian = True
+            entry_ref.update({"librarian": librarian})
+            print(f'\n"{username}" has been successfully edited.')
+            break
+        elif property == "3":
+            patron = input("Will this user have patron access (yes or no)? ")
+            if patron == "no":
+                patron = False
+            else:
+                patron = True
+            entry_ref.update({"patron": patron})
+            print(f'\n"{username}" has been successfully edited.')
+            break
+        else:
+            break
+
+# Only accessible to librarians.
+def display_librarian_control_panel(db):
     choice = None
-    while choice != "6":
+    while choice != "9":
         print()
         print("1) Add Book to Library")
         print("2) Remove Book from Library")
         print("3) Edit Book in Library")
         print("4) Display Books in Series")
         print("5) Display Books in Library")
-        print("6) Quit")
+        print("6) Add User")
+        print("7) Remove User")
+        print("8) Edit User")
+        print("9) Quit")
         choice = input("> ")
         print()
 
@@ -124,8 +167,76 @@ def main():
             display_books_by_series(db, series_name)
         elif choice == "5":
             display_books(db)
+        elif choice == "6":
+            username = input("What is the username you wish to add? ")
+            librarian = input("Will this user have libarian access (yes or no)? ")
+            if librarian == "no":
+                librarian = False
+            else:
+                librarian = True
+            patron = input("Will this user have patron access (yes or no)? ")
+            if patron == "no":
+                patron = False
+            else:
+                patron = True
+            set_user(db, username, librarian, patron)
+            print(f'\n"{username}" has been added.')
+        elif choice == "7":
+            username = input("What is the username you wish to remove? ")
+            remove_user(db, username)
+            print(f'\n"{username}" has been removed.')
+        elif choice == "8":
+            username = input("What is the username you wish to edit? ")
+            edit_user(db, username)
         else:
             break
 
+# Only accessible to patrons.
+def display_patron_control_panel(db):
+    choice = None
+    while choice != "3":
+        print()
+        print("1) Display Books in Series")
+        print("2) Display Books in Library")
+        print("3) Quit")
+        choice = input("> ")
+        print()
+
+        if choice == "1":
+            series_name = input("What is the name of the book series you wish to view? ")
+            display_books_by_series(db, series_name)
+        elif choice == "2":
+            display_books(db)
+        else:
+            break
+
+# Accessible to anyone who is not a librarian or patron.
+def display_unauthorized_user_control_panel():
+    print("Unauthorized User")
+    choice = None
+    while choice != "2":
+        print("1) Try another username")
+        print("2) Quit")
+        choice = input("> ")
+        print()
+
+        if choice == "1":
+            main(db)
+        else:
+            break
+
+def main(db):
+    user = input("Enter your username: ")
+    person = db.collection("users").document(f"{user}").get()
+    person = person.to_dict()
+
+    if person["librarian"] == True and person["patron"] == True:
+        display_librarian_control_panel(db)
+    elif person["librarian"] == False and person["patron"] == True:
+        display_patron_control_panel(db)
+    else:
+        display_unauthorized_user_control_panel()
+
 if __name__ == "__main__":
-    main()
+    db = initialize_firestore()
+    main(db)
